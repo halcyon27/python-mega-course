75d66b0e873f5e6704e8192179cf25d284403219
halcyon27 <halcyon_27@hotmail.com>
2016-12-20 17:46:20 -0800

    addiing code-section-2.py

---
+++ code-section-2.py
@@ -0,0 +1,9 @@
+# Notes from: Python-Mega-Course
+# FUNCTIONS
+# def to define a function; input passed in brackets
+
+def currency_converter(rate,euros):
+	dollars=euros*rate
+	return dollars
+	
+print(currency_converter(100,1000)
